ae31823600c085298d846ec768ac51a2d6215e15
Idris <idrisadebisi@live.com>
2019-11-26 23:39:40 -0500

    including gifs

--- schema.sql
+++ schema.sql
@@ -21,3 +21,9 @@ CREATE TABLE employee(
     role_id INT NOT NULL,
     manager_id DECIMAL,
 )
+
+INSERT INTO department (name) VALUES ('Human Resources'), ('Supply Chain'), ('Finanace'), ('Legal');
+INSERT INTO employee(firstName, lastName,role_id,manager_id) 
+VALUES ('Idris', 'Adebisi', 1, 1), ('Josephine', 'Shahab', 2, 1);
+INSERT INTO role(title, salary, department_id)
+VALUES ('Manager', 10.00, 20), ('Director',12.00,20), ('Intern', 7.00, 20); 
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-//const fs = require("fs");
-//const axios = require("axios");
+//Leave company 
+
+//UPDATE user SET companyId = NULL WHERE id = 1;
 const inquirer = require("inquirer");
 const express = require('express');
-//const consoleTable = require('console.table');
-//path
+
 
 const PORT = process.env.PORT || 8080;
 const mysql = require('mysql');
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// all our reservations in this
+
 app.use(express.static('public'));
 
 
@@ -50,29 +50,6 @@ const db = new Database({
     database: "employeesDB"
   });
   
-
-
-// let connection = mysql.createConnection({
-//   host: "localhost",
-
-//   // Your port; if not 3306
-//   port: 3306,
-
-//   // Your username
-//   user: "root",
-
-//   // Your password
-//   password: "testtest",
-//   database: "employeesDB"
-// });
-
-
-//  connection.connect( function(err) {
-//   if (err) throw err;
-//   console.log("connected as id " + connection.threadId);
-//    //connection.end();
-//    afterConnection();
-// })
 async function main(){
 
 departmentList = [];
@@ -85,7 +62,7 @@ let Emp_id;
     let employees = await db.query("SELECT * FROM employee");
         for (i = 0; i<employees.length; i++){
             employeeList.push(`${employees[i].firstName} ${employees[i].lastName}`);
-            //console.log(employeeList);
+
         }
 
         let departments = await db.query("SELECT * FROM department");
@@ -137,22 +114,6 @@ let Emp_id;
                                 ]
                         
                     },
-                    // {
-                    //     when: input => {
-                    //         return input.task == "Update employee information"
-                    //                     },
-
-
-                                        
-                    //         type: "list",
-                    //         name: "update",
-                    //         message: "What would you like to Update?",
-                    //         choices:[
-                    //             "Update employee information",
-                    //             "Update employee manager",
-                    //             ]
-                        
-                    // },
                     {
                         when: input => {
                             return input.task == "Delete departments, roles, employees"
@@ -168,7 +129,7 @@ let Emp_id;
                         
                     },
                 ]);
-    // now we have the inquirer results:
+ 
     if (response.add == "Add departments"){
         console.log("prepared to add department");
         inquirer
@@ -187,11 +148,7 @@ let Emp_id;
             })
     }
     else if (response.add == "Add roles"){
-        // console.log("prepared to add roles");
-        // let departments = await db.query("SELECT name FROM department");
-        // for (i = 0; i<departments.length; i++){
-        //     departmentList.push(departments[i].name);
-        // }
+        
         inquirer
         .prompt([
                 {
@@ -212,7 +169,7 @@ let Emp_id;
                 }
             ]).then(async (response)=>{
                 let dept_id = await db.query(`SELECT id FROM department WHERE name = '${response.addRole_department}'`);
-                //console.log(dept_id[0].id);
+                
                 await db.query(`INSERT INTO role (title, salary, department_id) VALUES ('${response.addRole}', ${response.addRole_salary}, ${dept_id[0].id})`);
                 console.log("New role added");
                 let addRole = await db.query("SELECT * FROM role");
@@ -223,10 +180,6 @@ let Emp_id;
     }
     else if (response.add == "Add employees"){
         console.log("prepared to add employees");
-        // let departments = await db.query("SELECT name, id FROM department");
-        // for (i = 0; i<departments.length; i++){
-        //     departmentList.push(departments[i].name);
-        // }
         
         await inquirer
         .prompt([
@@ -242,7 +195,7 @@ let Emp_id;
                 for (i = 0; i<roles.length; i++){
                 roleList.push(roles[i].title);
                 }
-                //console.log(Empdept_id[0].id);
+              
 
 
                 let managersInDept = await db.query(`SELECT employee.firstName, employee.lastName FROM employee INNER JOIN role ON employee.role_id = role.id WHERE title = "Manager" AND department_id = ${Empdept_id[0].id};`);
@@ -250,7 +203,7 @@ let Emp_id;
     
                 for (i = 0; i<managersInDept.length; i++){
                     managerList.unshift(`${managersInDept[i].firstName} ${managersInDept[i].lastName}`);
-                    //console.log(managerList);
+                   
                 }
             })
 
@@ -283,31 +236,22 @@ let Emp_id;
                 
                         let manager_firstName = response.addEmployee_manager.split(' ')[0];
                         let manager_lastName = response.addEmployee_manager.split(' ')[1];
-                        //console.log(manager_lastName, manager_firstName);
+            
                         let manager_id = await db.query(`SELECT id FROM employee WHERE firstName = '${manager_firstName}' AND lastName = '${manager_lastName}' `);
-                        //console.log(manager_id[0].id);
-
+                        
                         let role_id = await db.query(`SELECT id FROM role WHERE title = '${response.addEmployee_role}' AND department_id = ${Empdept_id[0].id}`);
-                        //console.log(role_id[0].id);
+        
                          await db.query(`INSERT INTO employee (firstName, lastName, role_id, manager_id) VALUES ('${response.addEmployee_firstName}', '${response.addEmployee_lastName}', ${role_id[0].id}, ${manager_id[0].id})`);
                          console.log("New employee added");
                          let addEmployee = await db.query("SELECT * FROM employee");
                          console.table(addEmployee);
                          main();
-
-
-
-                //console.log(response.addEmployee_lastName);
-                // await db.query(`INSERT INTO employee (firstName, lastName, role_id, manager_id) VALUES ('${response.addEmployee_firstName}', '${response.addEmployee_lastName}', ${role_id[0].id}, ${dept_id[0].id})`);
-                // console.log("New role added");
-                // let addRole = await db.query("SELECT * FROM role");
-                // console.table(addRole);
             });
     }
 
     if (response.view == "View departments"){
         console.log("prepared to view department");
-        //let departments = await db.query("SELECT * FROM department");
+      
         console.table(departments);
         main();
     }
@@ -319,7 +263,7 @@ let Emp_id;
     }
     else if (response.view == "View employees"){
         console.log("prepared to view employees");
-       //let employees = await db.query("SELECT * FROM employee",);
+       
        console.table(employees);
        main();
             
@@ -330,7 +274,7 @@ let Emp_id;
 
                 for (i = 0; i<mngrs.length; i++){
                     managerList.unshift(`${mngrs[i].firstName} ${mngrs[i].lastName}`);
-                    //console.log(managerList);
+                 
                 }
 
                 await inquirer
@@ -344,7 +288,6 @@ let Emp_id;
             ]).then(async (response)=>{
                         let manager_firstName = response.viewEmployee_manager.split(' ')[0];
                         let manager_lastName = response.viewEmployee_manager.split(' ')[1];
-                        //console.log(manager_lastName, manager_firstName);
                         let manager_id = await db.query(`SELECT id FROM employee WHERE firstName = '${manager_firstName}' AND lastName = '${manager_lastName}' `);
                         let employeeByManager = await db.query(`SELECT * FROM employee WHERE manager_id = ${manager_id[0].id} `);
                         console.table(employeeByManager);
@@ -354,37 +297,12 @@ let Emp_id;
     }
     else if (response.view == "View the total utilized budget of a department"){
         console.log("prepared to view the total utilized budget of a department");
-        // let departmnts = await db.query(`SELECT name FROM department`);
-
-        // for (i = 0; i<departmnts.length; i++){
-        //     departmentList.push(departmnts[i].name);
-        // }
-
-        // await inquirer
-        //         .prompt([
-        //         {
-        //             type: "list",
-        //             message: "Which departments utilized budget would you like to see?",
-        //             name: "viewDepartment_budget",
-        //             choices: departmentList
-        //         },
-        //     ])
         main();
 
     }
 
     if (response.task == "Update employee information"){
         console.log("prepared to update employee information");
-        // let employees = await db.query("SELECT firstName, lastName FROM employee");
-        // for (i = 0; i<employees.length; i++){
-        //     employeeList.push(`${employees[i].firstName} ${employees[i].lastName}`);
-        //     //console.log(employeeList);
-        // }
-
-        // let departments = await db.query("SELECT name FROM department");
-        // for (i = 0; i<departments.length; i++){
-        //     departmentList.push(departments[i].name);
-        // }
 
         await inquirer
                 .prompt([
@@ -411,7 +329,7 @@ let Emp_id;
                 for (i = 0; i<roles.length; i++){
                 roleList.push(roles[i].title);
                 }
-                //console.log(Empdept_id[0].id);
+            
 
 
                 let managersInDept = await db.query(`SELECT employee.firstName, employee.lastName FROM employee INNER JOIN role ON employee.role_id = role.id WHERE title = "Manager" AND department_id = ${Empdept_id[0].id};`);
@@ -419,7 +337,7 @@ let Emp_id;
     
                 for (i = 0; i<managersInDept.length; i++){
                     managerList.unshift(`${managersInDept[i].firstName} ${managersInDept[i].lastName}`);
-                    //console.log(managerList);
+                    
                 }
             })
 
@@ -441,10 +359,10 @@ let Emp_id;
                 let manager_firstName = response.updateEmployee_manager.split(' ')[0];
                 let manager_lastName = response.updateEmployee_manager.split(' ')[1];
                 let manager_id = await db.query(`SELECT id FROM employee WHERE firstName = '${manager_firstName}' AND lastName = '${manager_lastName}' `);
-                //console.log(manager_id[0].id);
+                
 
                 let role_id = await db.query(`SELECT id FROM role WHERE title = '${response.updateEmployee_role}' AND department_id = ${Empdept_id[0].id}`);
-                //console.log(role_id[0].id);
+            
                  await db.query(`UPDATE employee SET role_id = ${role_id[0].id}, manager_id = ${manager_id[0].id} WHERE id = ${Emp_id[0].id}`);
                  console.log("Employee record updated");
                  let updateEmployee = await db.query("SELECT * FROM employee");
@@ -455,15 +373,91 @@ let Emp_id;
     }
 
     if (response.delete == "Delete departments"){
-        console.log("prepared to delete department");
+
+        await inquirer
+                .prompt([
+                {
+                    type: "list",
+                    message: "Please select department to delete?",
+                    name: "deleteDepartment",
+                    choices: departmentList
+                },
+            ]).then(async (response)=>{
+                let dept_id = await db.query(`SELECT id FROM department WHERE name = '${response.deleteDepartment}'`);
+                await db.query(`DELETE FROM department WHERE id = ${dept_id[0].id} `);
+                let departments = await db.query("SELECT * FROM department");
+                console.log("Selected department deleted");
+                console.table(departments);
+                main();
+            })
+
     }
     else if (response.delete == "Delete roles"){
         console.log("prepared to delete roles");
+
+        await inquirer
+        .prompt([
+                {
+                    type: "list",
+                    message: "What department does the role to be deleted belong to",
+                    name: "deleteRole_department",
+                    choices: departmentList
+                }
+            ]).then(async (response)=>{
+                let roles = await db.query(`SELECT title FROM role INNER JOIN department ON role.department_id = department.id WHERE department.name = '${response.deleteRole_department}' `);
+                Empdept_id = await db.query(`SELECT id FROM department WHERE name = '${response.deleteRole_department}'`);
+                for (i = 0; i<roles.length; i++){
+                roleList.push(roles[i].title);
+                }
+
+            })
+
+            await inquirer
+            .prompt([
+                    
+                    {
+                        type: "list",
+                        message: "Please select role",
+                        name: "deleteRole",
+                        choices: roleList
+                    }
+                    
+                ]).then(async (response)=>{
+
+                let role_id = await db.query(`SELECT id FROM role WHERE title = '${response.deleteRole}' AND department_id = ${Empdept_id[0].id}`);
+                
+                 await db.query(`DELETE FROM role WHERE id = ${role_id[0].id} `);
+                 console.log("Deleted selected role");
+                 let deleteRole = await db.query("SELECT * FROM role");
+                 console.table(deleteRole);
+                 main();
+                })
     }
     else if (response.delete == "Delete employees"){
         console.log("prepared to delete employees");
-    }
+
+
+
+        await inquirer
+                .prompt([
+                {
+                    type: "list",
+                    message: "Which employee would you like to Delete?",
+                    name: "deleteEmployee",
+                    choices: employeeList
+                }
+            ]).then(async (response)=>{
+
+                let employee_firstName = response.deleteEmployee.split(' ')[0];
+                let employee_lastName = response.deleteEmployee.split(' ')[1];
+                Emp_id = await db.query(`SELECT id from employee WHERE firstName = '${employee_firstName}' AND lastName = '${employee_lastName}'`);
+                 await db.query(`DELETE FROM employee WHERE id = ${Emp_id[0].id} `);
+                 console.log("Deleted selected employee");
+                 let deleteEmployee = await db.query("SELECT * FROM employee");
+                 console.table(deleteEmployee);
+                 main();
+                })
+}
 }
 
 main();
-       // }
